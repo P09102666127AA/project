6164a8d1efeb30d318225c551a6fc0c32ea016a6
pooria <pa2002.tir@gmail.com>
2023-05-29 17:44:11 +0330

    message

---
+++ components/image.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="flex items-center cursor-pointer mt-2 mb-2">
+      <img class="inline-block h-24 w-24 mt-10 ml-10 rounded-full " src="logo.png" alt="logo"/>
+    </div>
+</template>
+
+<script>
+export default {
+
+  }
+
+</script>
