b3183df0a7f733e66fcc2072aaffaf35f478a69a
pooria <pa2002.tir@gmail.com>
2023-06-07 11:33:46 +0330

    new edit

--- components/pagetourcommponent.vue
+++ components/pagetourcommponent.vue
@@ -1,63 +1,5 @@
 <template>
   <div>
-
-    <div class="flex items-center flex flex-col md:flex-row lg:flex-row sm:flex-col mt-32 ">
-  <img src="international-tour.jpg"  class="w-32 h-32 md:w-60 md:h-60 lg:w-80 lg:h-80 mr-10 ml-10">
-  <div>
-    <strong class="text-black mr-20 text-right leading-loose justify-center ml-20">تور استانبول یکی از بهترین سفرهای شما برای دیدن آسیا و اروپا است. شهری
-        با مساجد و طبیعت خیره کننده همراه با کافه ها و رستوران هایی که با بسیار
-        باب میل ایرانیان است. این شهر دروازه اقتصادی ترکیه بوده و فصول مختلف
-        توریست های بسیاری با تهیه بلیط ارزان قیمت استانبول به شوق خرید در
-        بازارهای سنتی و مراکز خرید مدرن استانبول پا به این شهر می‌گذارند. بلیط
-        ارزان هواپیما استانبول را می توانید از پکیج های تور استانبول که توسط
-        <strong class="text-orange-500">کمپانی بدرقه پرواز آرین</strong>
-        ارایه می شود تهیه کنید.</strong>
-    <br>
-    <span class="text-black mr-20 text-right leading-loose justify-center ml-20"></span>
-  </div>
-</div>
-
-<div class="flex flex-col md:flex-row-reverse lg:flex-row-reverse sm:flex-col mt-32 items-center ">
-  <img src="international-tour.jpg"  class="w-32 h-32 md:w-60 md:h-60 lg:w-80 lg:h-80 mr-10 ml-10">
-  <div>
-    <p class="text-black mr-20 text-right leading-loose justify-center ml-20">
-     خدماتی که بدرقه پرواز آرین به شما ارائه می دهد
-      </p>
-    <br>
-    <span class="text-black mr-20 text-right leading-loose justify-center ml-20">رزرو سه شب هتل</span>
-    <br>
-    <span class="text-black mr-20 text-right leading-loose justify-center ml-20">اشنایی با اماکن تفریحی</span>
-    <br>
-    <span class="text-black mr-20 text-right leading-loose justify-center ml-20">ارائه گذرنامه</span>
-  </div>
-</div>
-
-
-
-
-
-
-
-
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-
-
-
-
-
-
     <h1 class="text-orange-600 text-center mt-10 font-bold text-2xl">
       تور استانبول
     </h1>
@@ -68,46 +10,51 @@
       شهرها
     </p>
 
-
-    <div class="flex flex-col md:flex-row lg:flex-row sm:flex-col mt-32 items-center">
-      <p class="text-black mr-20 text-right leading-loose justify-center ml-20">
-        it is testttt
-      </p>
-      <br>
-      <p class="text-black mr-20 text-right leading-loose justify-center ml-20">
-        تور استانبول یکی از بهترین سفرهای شما برای دیدن آسیا و اروپا است. شهری
-        با مساجد و طبیعت خیره کننده همراه با کافه ها و رستوران هایی که با بسیار
-        باب میل ایرانیان است. این شهر دروازه اقتصادی ترکیه بوده و فصول مختلف
-        توریست های بسیاری با تهیه بلیط ارزان قیمت استانبول به شوق خرید در
-        بازارهای سنتی و مراکز خرید مدرن استانبول پا به این شهر می‌گذارند. بلیط
-        ارزان هواپیما استانبول را می توانید از پکیج های تور استانبول که توسط
-        <strong class="text-orange-500">کمپانی بدرقه پرواز آرین</strong>
-        ارایه می شود تهیه کنید.
-      </p>
+    <div
+      class="flex items-center flex flex-col md:flex-row lg:flex-row sm:flex-col mt-32"
+    >
       <img
-        class="w-32 h-32 md:w-60 md:h-60 lg:w-80 lg:h-80 mr-10 ml-10"
         src="international-tour.jpg"
+        class="w-32 h-32 md:w-60 md:h-60 lg:w-80 lg:h-80 mr-10 ml-10"
       />
+      <div>
+        <p
+          class="text-black mr-20 text-right leading-loose justify-center ml-20 text-bold"
+        >
+          تور استانبول یکی از بهترین سفرهای شما برای دیدن آسیا و اروپا است. شهری
+          با مساجد و طبیعت خیره کننده همراه با کافه ها و رستوران هایی که با
+          بسیار باب میل ایرانیان است. این شهر دروازه اقتصادی ترکیه بوده و فصول
+          مختلف توریست های بسیاری با تهیه بلیط ارزان قیمت استانبول به شوق خرید
+          در بازارهای سنتی و مراکز خرید مدرن استانبول پا به این شهر می‌گذارند.
+          بلیط ارزان هواپیما استانبول را می توانید از پکیج های تور استانبول که
+          توسط
+          <strong class="text-orange-500">کمپانی بدرقه پرواز آرین</strong>
+          ارایه می شود تهیه کنید.
+        </p>
+        <br />
+      </div>
     </div>
-    <h1 class="text-orange-500 block ">it is test</h1>
-    <div class="flex flex-col md:flex-row-reverse lg:flex-row-reverse sm:flex-col mt-32 items-center">
-      <h1 class="text-orange-500 block ">it is test</h1>
-      <p class="text-black mr-20 text-right leading-loose justify-center ml-20">
-        استانبول بزرگ‌ترین و پرجمعیت‌ترین شهر در کشور ترکیه و مرکز فرهنگی و
-        اقتصادی این کشور است. جمعیت این شهر در آمار سال ۲۰۱۸ حدود ۱۵٬۰۶۷٬۷۲۴ نفر
-        گزارش شده‌است. استانبول سومین کلان‌شهر بزرگ خاورمیانه محسوب می‌شود.
-        همچنین این شهر بزرگترین کلان‌شهر اروپا می‌باشد. این شهر در کنار تنگه
-        بسفر، دریای مرمره و دریای سیاه قرار دارد. ین شهر در کنار تنگه بسفر،
-        دریای مرمره و دریای سیاه قرار دارد. تنگه بسفر دو قارهٔ آسیا و اروپا را
-        از یکدیگر جدا می‌کند و استانبول تنها شهر بزرگ جهان است که در دو قارهٔ
-        جهان گسترده شده‌است. بندر طبیعی شاخ طلایی یا خلیج در این شهر واقع است.
-        بر مبنای آخرین داده های دقیق جغرافیایی کلانشهر استانبول ۷۲ کیلومتر درازا
-        دارد.
-      </p>
+
+    <div
+      class="flex flex-col md:flex-row-reverse lg:flex-row-reverse sm:flex-col mt-32 items-center mb-10"
+    >
       <img
-        class="w-32 h-32 md:w-60 md:h-60 lg:w-80 lg:h-80 mr-10 ml-10 mb-10"
-        src="tour setanbol.jpg"
+        src="international-tour.jpg"
+        class="w-32 h-32 md:w-60 md:h-60 lg:w-80 lg:h-80 mr-10 ml-10"
       />
+      <div class="p-4">
+        <div class="flow-root">
+          <div class="my-4 text-2xl text-orange-500 ml-24">
+            خدماتی که کمپانی بدرقه پرواز آرین در این راه به شما ارائه می دهد: 
+          </div>
+        </div>
+        <ul class="flow-root">
+          <li class="my-4">ارائه گذرنامه موقت</li>
+          <li class="my-4">معرفی اماکن تفریحی</li>
+          <li class="my-4">تهیه مواد خوراکی</li>
+          <li class="my-4">تهیه ارامش مسافران</li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
