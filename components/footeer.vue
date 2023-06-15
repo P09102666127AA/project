81b8342726df5484770907cc2eae47e8995ab949
pooria <pa2002.tir@gmail.com>
2023-06-15 14:16:29 +0330

    edit on like email and phone and edit bot email

--- components/footeer.vue
+++ components/footeer.vue
@@ -22,8 +22,8 @@
           <p class="text-white flex justify-center mt-10 text-lg">ما را در فضای مجازی دنبال کنید:</p>
           <div class="flex justify-center mt-4">
           <div class="mt-10 mb-6">
-            <button
-              type="button"
+            <a
+              href="https://instagram.com/badraghe_parvaz?igshid=MzRlODBiNWFlZA=="
               data-te-ripple-init
               data-te-ripple-color="light"
               class="mb-2 inline-block rounded-full p-3 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg bg-rose-600 hover:bg-indigo-600"
@@ -38,7 +38,7 @@
                   d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"
                 />
               </svg>
-            </button>
+            </a>
           </div>
           <div class="mt-10 mb-6 mr-10">
             <button
@@ -121,14 +121,14 @@
           <ul>
             <li class="text-white py-2">راه های ارتباطی با ما:</li>
             <li class="text-white py-2">
-              <a href="#" class="hover:text-black">86043550</a> _
-              <a href="#" class="hover:text-black">09126118386</a>
+              <a href="tel:+98 02186043550" class="hover:text-black">86043550</a> _
+              <a href="tel:09126118386" class="hover:text-black">09126118386</a>
             </li>
             <li class="text-white py-2">
-              <a href="#" class="hover:text-black">Admin@gmail.com</a>
+              <a href="mailto:info@badraghe.me" class="hover:text-black">info@badraghe.me</a>
             </li>
             <li class="text-white pt-2">آدرس</li>
-            <li class="text-white pb-4">شریعتی _ هویزه</li>
+            <li class="text-white pb-4"> سهروردی شمالی خیابان هویزه پلاک 53 واحد 1 زنگ 4 </li>
           </ul>
         </div>
       </div>
--- components/iconphone.vue
+++ components/iconphone.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-row-reverse ml-10 mb-10">
     <a
-      href="tel:+5212288558"
+      href="tel:+98 02186043550"
       class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-bold rounded-full p-4  dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
     >
       <svg
